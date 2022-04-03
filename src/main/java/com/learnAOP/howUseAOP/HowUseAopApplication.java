06f95a13d568d95eaca98e356099769a7d552ca1
tgoetter <Gittobi471dafbmkot>
2022-04-03 17:10:20 -0300

    Criação dos arquivos do projeto.

---
+++ src/main/java/com/learnAOP/howUseAOP/HowUseAopApplication.java
@@ -0,0 +1,15 @@
+package com.learnAOP.howUseAOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class HowUseAopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HowUseAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnAOP/howUseAOP/HowUseAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnAOP.howUseAOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowUseAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
